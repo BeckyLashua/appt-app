84998d6d9d706ea84eac7d810c2dec9639fd7ea5
BeckyLashua <beckylashua@gmail.com>
2024-05-12 17:24:14 -0500

    Added basic search routing mock

--- src/server.js
+++ src/server.js
@@ -17,6 +17,27 @@ app.get('/api/message', (req, res) => {
   }
 });
 
+
+// search/appointments page handler 
+// mock a response to a database read
+app.get('/api/search', (req, res) => {
+  try {
+    res.json({ message: '[appt data array]!' });
+  } catch (error) {
+    console.error('Failed to send message:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// booking page handler
+// mock response to a database create
+
+// reschedule page handler
+// mock the response to a database update
+
+// cancel handler
+// mock the response to a database delete
+
 // Start the server
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
