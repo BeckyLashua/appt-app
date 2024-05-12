01f43f311cab3db9a12a170e17a24f8db0d53c29
BeckyLashua <beckylashua@gmail.com>
2024-05-12 13:37:56 -0500

    Switched to utilizing  expess

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3001;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+// Define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
+// Start the server
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
